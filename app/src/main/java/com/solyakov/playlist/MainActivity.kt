0f6bb6392d3ae2e659333ee7df315e3d0acd47a1
Langod <yuracolicov@gmail.com>
2025-10-15 12:34:42 +0300

    Add StartScreen

--- app/src/main/java/com/solyakov/playlist/MainActivity.kt
+++ app/src/main/java/com/solyakov/playlist/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.solyakov.playlist.presentation.MainScreen
 import com.solyakov.playlist.ui.theme.PlaylistTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlaylistTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlaylistTheme {
-        Greeting("Android")
     }
 }
